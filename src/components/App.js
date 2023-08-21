8347424674a444faf04b8b17490cf9a5190fa0b7
Rithikeshh <135729296+Rithikeshh@users.noreply.github.com>
2023-08-21 23:54:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
-
+  const [arr,setArr] = React.useState([]);
+  function addDivWithShape(event){
+    let shapeValue = event.target.parentNode.children[0].value;
+    setArr((oldArr)=>{
+      const copyArr = [...oldArr,<div className={shapeValue.toLowerCase()}>{shapeValue}</div>]
+      return copyArr;
+    })
+  }
   return (
     <div id="main">
       <div id="shape-creator">
-        
-
+        <select>
+          <option>Square</option>
+          <option>Circle</option>
+        </select>
+        <button onClick={addDivWithShape}>Add Shape</button>
       </div>
       <div id="shapes-holder">
+        {arr}
       </div>
     </div>
   )
